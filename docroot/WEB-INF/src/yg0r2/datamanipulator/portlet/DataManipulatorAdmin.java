7f71dcd096c90580d768e55917ad1110e119051b
Yg0R2 <kovacs1tibor@gmail.com>
2015-03-13 12:07:43 +0100

    Add back-end

---
+++ docroot/WEB-INF/src/yg0r2/datamanipulator/portlet/DataManipulatorAdmin.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2014-present Yg0R2. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package yg0r2.datamanipulator.portlet;
+
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import yg0r2.datamanipulator.context.RequestContext;
+import yg0r2.datamanipulator.handler.BaseHandler;
+import yg0r2.datamanipulator.handler.util.HandlerUtil;
+import yg0r2.datamanipulator.runner.DataManipulatorRunner;
+/**
+ * @author Yg0R2
+ */
+public class DataManipulatorAdmin extends MVCPortlet {
+
+	public void create(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception { 
+
+		UploadPortletRequest uploadRequest = PortalUtil.getUploadPortletRequest(
+			actionRequest);
+
+		RequestContext requestContext = new RequestContext(uploadRequest);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)uploadRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String handlerName = ParamUtil.getString(uploadRequest, "handlerName");
+
+		BaseHandler handler = HandlerUtil.getHandler(handlerName);
+
+		boolean handlerIsPortalType = HandlerUtil.isPortalType(handlerName);
+
+		long[] groupIds = requestContext.getGroupIds();
+
+		if ((groupIds.length <= 0) || handlerIsPortalType) {
+			groupIds = new long[] {themeDisplay.getScopeGroupId()};
+		}
+
+		long[] userIds = requestContext.getUserIds();
+
+		if ((userIds.length <= 0) || handlerIsPortalType) {
+			userIds = new long[] {themeDisplay.getUserId()};
+		}
+
+		for (long groupId : groupIds) {
+			for (long userId : userIds) {
+				requestContext = new RequestContext(uploadRequest);
+
+				requestContext.setGroupId(groupId);
+				requestContext.setUserId(userId);
+
+				DataManipulatorRunner runner = new DataManipulatorRunner(
+					handler, requestContext);
+
+				runner.start();
+			}
+		}
+	}
+
+}
---
+++ docroot/WEB-INF/src/yg0r2/datamanipulator/runner/DataManipulatorRunner.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2014-present Yg0R2. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package yg0r2.datamanipulator.runner;
+
+import javax.servlet.http.HttpSession;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.PortalSessionThreadLocal;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.security.permission.PermissionThreadLocal;
+
+import yg0r2.datamanipulator.context.RequestContext;
+import yg0r2.datamanipulator.handler.BaseHandler;
+/**
+ * @author Yg0R2
+ */
+public class DataManipulatorRunner extends Thread {
+
+	public DataManipulatorRunner(
+		BaseHandler handler, RequestContext requestContext) {
+
+		_handler = handler;
+		_requestContext = requestContext;
+	}
+
+	@Override
+	public void run() {
+		HttpSession httpSession = _requestContext.getSession();
+		httpSession.setMaxInactiveInterval(-1);
+
+		PermissionThreadLocal.setIndexEnabled(false);
+		PermissionThreadLocal.setPermissionChecker(
+			_requestContext.getPermissionChecker());
+
+		PrincipalThreadLocal.setName(_requestContext.getUserId());
+
+		PortalSessionThreadLocal.setHttpSession(httpSession);
+
+		String threadName = (Thread.currentThread()).getName();
+
+		long startTime = System.currentTimeMillis();
+
+		_log.info(
+			threadName + " start add entries at " + String.valueOf(startTime) +
+			StringPool.PERIOD);
+
+		try {
+			_handler.proceed(_requestContext);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		long endTime = System.currentTimeMillis();
+
+		_log.info(
+			threadName + " finished add entries at " + String.valueOf(endTime) +
+			StringPool.PERIOD);
+
+		double taked = (endTime - startTime) / 1000;
+
+		_log.info("The whole process taked " + String.valueOf(taked) + "s.");
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		DataManipulatorRunner.class);
+
+	private BaseHandler _handler;
+	private RequestContext _requestContext;
+
+}
