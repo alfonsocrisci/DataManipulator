59a10b785fd473e162de57ded403c6af63d92cae
Yg0R2 <kovacs1tibor@gmail.com>
2015-03-13 12:10:37 +0100

    Add 'portal' Handlers

---
+++ docroot/WEB-INF/src/yg0r2/datamanipulator/handler/portal/SiteHandler.java
@@ -0,0 +1,336 @@
+/**
+ * Copyright (c) 2014-present Yg0R2. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package yg0r2.datamanipulator.handler.portal;
+
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.staging.StagingUtil;
+import com.liferay.portal.kernel.util.KeyValuePair;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.LayoutSetPrototype;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.LayoutSetPrototypeLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import yg0r2.datamanipulator.annotation.Handler;
+import yg0r2.datamanipulator.annotation.HandlerType;
+import yg0r2.datamanipulator.context.RequestContext;
+import yg0r2.datamanipulator.displayfield.DisplayFields;
+import yg0r2.datamanipulator.displayfield.FieldKeys;
+import yg0r2.datamanipulator.handler.BaseHandler;
+import yg0r2.datamanipulator.handler.content.LayoutHandler;
+import yg0r2.datamanipulator.handler.util.HandlerUtil;
+
+/**
+ * @author Yg0R2
+ */
+@Handler(type = HandlerType.PORTAL, displayName = "Site Handler")
+public class SiteHandler extends BaseHandler {
+
+	public static final String ACTIVATE_STAGING_CHECKBOX =
+		"activate-staging-checkbox";
+
+	public static final String ADD_HOME_PAGE_CHECKBOX =
+		"add-home-page-checkbox";
+
+	public static final String SITE_CLASS_NAME = "site-class-name";
+	public static final String SITE_CLASS_PK = "site-class-pk";
+	public static final String SITE_TEMPLATE_LIST = "site-template-list";
+	public static final String SITE_TYPE_SELECT_LIST = "site-type-select-list";
+
+	public SiteHandler() throws Exception {
+		super("Site", "site");
+
+		ClassLoader portalClassLoader = PortalClassLoaderUtil.getClassLoader();
+
+		updateLayoutSetPrototypesLinksClass = portalClassLoader.loadClass(
+			"com.liferay.portlet.sites.util.SitesUtil");
+
+		updateLayoutSetPrototypesLinksMethod =
+			updateLayoutSetPrototypesLinksClass.getMethod(
+				"updateLayoutSetPrototypesLinks",
+				new Class<?>[] {
+					Group.class, Long.TYPE, Long.TYPE, Boolean.TYPE,
+					Boolean.TYPE
+				});
+	}
+
+	private Class<?> updateLayoutSetPrototypesLinksClass;
+	private Method updateLayoutSetPrototypesLinksMethod;
+
+	@Override
+	public Object addEntry(RequestContext requestContext) throws Exception {
+		Group group = (Group) super.addEntry(requestContext);
+
+		long privateLayoutSetPrototypeId =
+			requestContext.getLong(
+				getDisplayFieldName("private-" + SITE_TEMPLATE_LIST));
+
+		long publicLayoutSetPrototypeId =
+			requestContext.getLong(
+				getDisplayFieldName("public-" + SITE_TEMPLATE_LIST));
+
+		updateLayoutSetPrototypesLinksMethod.invoke(
+			updateLayoutSetPrototypesLinksClass,
+			new Object[] {
+				group, publicLayoutSetPrototypeId, privateLayoutSetPrototypeId,
+				true, false
+			});
+
+		UserLocalServiceUtil.addGroupUsers(
+			group.getGroupId(), requestContext.getUserIds());
+
+		boolean addHomePage = requestContext.getBoolean(
+			getDisplayFieldName(ADD_HOME_PAGE_CHECKBOX));
+
+		if (addHomePage) {
+			RequestContext layoouRequestContext = requestContext.clone();
+
+			layoouRequestContext.setGroupId(group.getGroupId());
+
+			LayoutHandler layoutHandler = new LayoutHandler();
+
+			layoutHandler.addEntry(layoouRequestContext);
+		}
+
+		boolean activateStaging = requestContext.getBoolean(
+			getDisplayFieldName(ACTIVATE_STAGING_CHECKBOX));
+
+		if (activateStaging) {
+			long userId = requestContext.getUserId();
+
+			StagingUtil.enableLocalStaging(
+				userId, null, group, false, false,
+				HandlerUtil.getServiceContext(group.getGroupId(), userId));
+		}
+
+		return group;
+	}
+
+	@Override
+	public DisplayFields getDisplayFields(long groupId) throws SystemException {
+		DisplayFields displayFields = new DisplayFields();
+
+		displayFields.addLabel(getDisplayFieldName());
+
+		displayFields.addUserMultiSelect(FieldKeys.MULTI_SELECT_USER_LIST);
+
+		displayFields.addSelectList(
+			getDisplayFieldName(SITE_TYPE_SELECT_LIST), _getSiteTypes());
+
+		displayFields.addSelectList(
+			getDisplayFieldName("public-" + SITE_TEMPLATE_LIST),
+			_getSiteTemplates());
+
+		displayFields.addSelectList(
+			getDisplayFieldName("private-" + SITE_TEMPLATE_LIST),
+			_getSiteTemplates());
+
+		displayFields.addCheckbox(
+			getDisplayFieldName(ADD_HOME_PAGE_CHECKBOX), true);
+
+		displayFields.addCheckbox(
+			getDisplayFieldName(ACTIVATE_STAGING_CHECKBOX), false);
+
+		displayFields.addCount(
+			getDisplayFieldName(FieldKeys.INPUT_COUNT), true);
+
+		if (buildNumber >= 6200) {
+			displayFields.addDepth(
+				getDisplayFieldName(FieldKeys.INPUT_DEPTH));
+
+			displayFields.addSubCount(
+				getDisplayFieldName(FieldKeys.INPUT_SUBCOUNT));
+		}
+
+		return displayFields;
+	}
+
+	@Override
+	protected Class<?>[] getAddArgClazzs() {
+		return new Class<?>[] {
+			Long.TYPE, String.class, Long.TYPE, Long.TYPE, String.class,
+			String.class, Integer.TYPE, String.class, Boolean.TYPE,
+			Boolean.TYPE, ServiceContext.class
+		};
+	}
+
+	@Override
+	protected String[] getAddArgNames() {
+		return new String[] {
+			"userId", "className", "classPK", "liveGroupId", "name",
+			"description", "type", "friendlyURL", "site", "active",
+			"serviceContext"
+		};
+	}
+
+	@Override
+	protected Class<?> getAddClass() {
+		return GroupLocalServiceUtil.class;
+	}
+
+	@Override
+	protected Map<String, Object> getAddEntrySpecifiedArgs(
+		RequestContext requestContext) throws Exception {
+
+		String className;
+		if (requestContext.contains(getDisplayFieldName(SITE_CLASS_NAME))) {
+			className = requestContext.getString(
+				getDisplayFieldName(SITE_CLASS_NAME));
+		}
+		else {
+			className = Group.class.getName();
+		}
+
+		long classPK;
+		if (requestContext.contains(getDisplayFieldName(SITE_CLASS_PK))) {
+			classPK = requestContext.getLong(
+				getDisplayFieldName(SITE_CLASS_PK));
+		}
+		else {
+			classPK = (long)0;
+		}
+
+		int type = requestContext.getInteger(
+			getDisplayFieldName(SITE_TYPE_SELECT_LIST),
+			GroupConstants.TYPE_SITE_OPEN);
+
+		Map<String, Object> args = new HashMap<String, Object>(6);
+
+		args.put("active", true);
+		args.put("className", className);
+		args.put("classPK", classPK);
+		args.put("liveGroupId", GroupConstants.DEFAULT_LIVE_GROUP_ID);
+		args.put("site", true);
+		args.put("type", type);
+
+		return args;
+	}
+
+	@Override
+	protected String getAddMethodName() {
+		return "addGroup";
+	}
+
+	@Override
+	protected List<String> getChildHandlerNames() {
+		return new ArrayList<String>(0);
+	}
+
+	@Override
+	protected Object getClassPK(Object entry) {
+		if (Validator.isNull(entry)) {
+			return 0;
+		}
+
+		return ((Group)entry).getClassPK();
+	}
+
+	@Override
+	protected String getClassPKName() {
+		return "groupId";
+	}
+
+	@Override
+	protected String getParentClassPKName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getUpdateArgClazzs() {
+		return new Class<?>[] {
+			Long.TYPE, String.class, String.class, Integer.TYPE, String.class,
+			Boolean.TYPE, ServiceContext.class
+		};
+	}
+
+	@Override
+	protected String[] getUpdateArgNames() {
+		return new String[] {
+			"groupId", "name", "description", "type", "friendlyURL", "active",
+			"serviceContext"
+		};
+	}
+
+	@Override
+	protected Class<?> getUpdateClass() {
+		return GroupLocalServiceUtil.class;
+	}
+
+	@Override
+	protected Map<String, Object> getUpdateEntrySpecifiedArgs(
+		Object entry, RequestContext requestContext) throws Exception {
+
+		return new HashMap<String, Object>(0);
+	}
+
+	@Override
+	protected String getUpdateMethodName() {
+		return "updateGroup";
+	}
+
+	private List<KeyValuePair> _getSiteTemplates() throws SystemException {
+		List<KeyValuePair> siteTemplates = new ArrayList<KeyValuePair>();
+
+		siteTemplates.add(new KeyValuePair("", ""));
+
+		List<LayoutSetPrototype> layoutSetPrototypes =
+			LayoutSetPrototypeLocalServiceUtil.getLayoutSetPrototypes(
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		for (LayoutSetPrototype layoutSetPrototype : layoutSetPrototypes) {
+			siteTemplates.add(
+				new KeyValuePair(
+					layoutSetPrototype.getName(),
+					String.valueOf(
+						layoutSetPrototype.getLayoutSetPrototypeId())));
+		}
+
+		return siteTemplates;
+	}
+
+	private  List<KeyValuePair> _getSiteTypes() throws SystemException {
+		List<KeyValuePair> siteTypes = new ArrayList<KeyValuePair>();
+
+		siteTypes.add(
+			new KeyValuePair(
+				GroupConstants.TYPE_SITE_OPEN_LABEL,
+				String.valueOf(GroupConstants.TYPE_SITE_OPEN)));
+
+		siteTypes.add(
+			new KeyValuePair(
+				GroupConstants.TYPE_SITE_PRIVATE_LABEL,
+				String.valueOf(GroupConstants.TYPE_SITE_PRIVATE)));
+
+		siteTypes.add(
+			new KeyValuePair(
+				GroupConstants.TYPE_SITE_RESTRICTED_LABEL,
+				String.valueOf(GroupConstants.TYPE_SITE_RESTRICTED)));
+
+		return siteTypes;
+	}
+
+}
---
+++ docroot/WEB-INF/src/yg0r2/datamanipulator/handler/portal/UserHandler.java
@@ -0,0 +1,219 @@
+/**
+ * Copyright (c) 2014-present Yg0R2. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package yg0r2.datamanipulator.handler.portal;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import yg0r2.datamanipulator.annotation.Handler;
+import yg0r2.datamanipulator.annotation.HandlerType;
+import yg0r2.datamanipulator.context.RequestContext;
+import yg0r2.datamanipulator.displayfield.DisplayFields;
+import yg0r2.datamanipulator.displayfield.FieldKeys;
+import yg0r2.datamanipulator.handler.BaseHandler;
+/**
+ * @author Yg0R2
+ */
+@Handler(type = HandlerType.PORTAL, displayName = "User Handler")
+public class UserHandler extends BaseHandler {
+
+	public UserHandler() {
+		super("User", "user");
+	}
+
+	@Override
+	public DisplayFields getDisplayFields(long groupId) throws SystemException {
+		DisplayFields displayFields = new DisplayFields();
+
+		displayFields.addLabel(getDisplayFieldName());
+
+		displayFields.addOrganizationMultiSelect(
+			FieldKeys.MULTI_SELECT_ORGANIZATION_LIST);
+
+		displayFields.addRoleMultiSelect(FieldKeys.MULTI_SELECT_ROLE_LIST);
+
+		displayFields.addSiteMultiSelect(FieldKeys.MULTI_SELECT_SITE_LIST);
+
+		displayFields.addUserGroupMultiSelect(
+			FieldKeys.MULTI_SELECT_USERGROUP_LIST);
+
+		displayFields.addCount(
+			getDisplayFieldName(FieldKeys.INPUT_COUNT), true);
+
+		return displayFields;
+	}
+
+	@Override
+	protected Class<?>[] getAddArgClazzs() {
+		return new Class<?>[] {
+			Long.TYPE, Long.TYPE, Boolean.TYPE, String.class, String.class,
+			Boolean.TYPE, String.class, String.class, Long.TYPE, String.class,
+			Locale.class, String.class, String.class, String.class,
+			Integer.TYPE, Integer.TYPE, Boolean.TYPE, Integer.TYPE,
+			Integer.TYPE, Integer.TYPE, String.class,
+			Array.newInstance(Long.TYPE, 0).getClass(),
+			Array.newInstance(Long.TYPE, 0).getClass(),
+			Array.newInstance(Long.TYPE, 0).getClass(),
+			Array.newInstance(Long.TYPE, 0).getClass(), Boolean.TYPE,
+			ServiceContext.class
+		};
+	}
+
+	@Override
+	protected String[] getAddArgNames() {
+		return new String[] {
+			"creatorUserId", "companyId", "autoPassword", "password1",
+			"password2", "autoScreenName", "screenName", "emailAddress",
+			"facebookId", "openId", "locale", "firstName", "middleName",
+			"lastName", "prefixId", "suffixId", "male", "birthdayMonth",
+			"birthdayDay", "birthdayYear", "jobTitle", "groupIds",
+			"organizationIds", "roleIds", "userGroupIds", "sendEmail",
+			"serviceContext"
+		};
+	}
+
+	@Override
+	protected Class<?> getAddClass() {
+		return UserLocalServiceUtil.class;
+	}
+
+	@Override
+	protected Map<String, Object> getAddEntrySpecifiedArgs(
+		RequestContext requestContext) throws Exception {
+
+		StringBuilder postSB = new StringBuilder(3);
+		postSB.append(requestContext.getString("entryCount"));
+		postSB.append(StringPool.DASH);
+		postSB.append(requestContext.getString("rndString"));
+
+		String postString = postSB.toString();
+
+		String screenName = "test-" + postString;
+
+		Map<String, Object> args = new HashMap<String, Object>(16);
+
+		args.put("creatorUserId", requestContext.getUserId());
+		args.put("password1", "test");
+		args.put("password2", "test");
+		args.put("screenName", screenName);
+		args.put("emailAddress", screenName + "@liferaz.com");
+		args.put("firstName", "Test" + postString);
+		args.put("lastName", "Test" + postString);
+		args.put("birthdayMonth", 10);
+		args.put("birthdayDay", 16);
+		args.put("birthdayYear", 1984);
+		args.put("male", true);
+		args.put("jobTitle", "QA Engineer");
+
+		args.put("groupIds", requestContext.getGroupIds());
+		args.put("organizationIds", requestContext.getOrganizationIds());
+		args.put("roleIds", requestContext.getRoleIds());
+		args.put("userGroupIds", requestContext.getUserGroupIds());
+
+		return args;
+	}
+
+	@Override
+	protected String getAddMethodName() {
+		return "addUser";
+	}
+
+	@Override
+	protected List<String> getChildHandlerNames() {
+		return new ArrayList<String>(0);
+	}
+
+	@Override
+	protected Object getClassPK(Object entry) {
+		if (Validator.isNull(entry)) {
+			return 0;
+		}
+
+		return ((User)entry).getUserId();
+	}
+
+	@Override
+	protected String getClassPKName() {
+		return "userId";
+	}
+
+	@Override
+	protected String getParentClassPKName() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getUpdateArgClazzs() {
+		return new Class<?>[] {
+			Long.TYPE, String.class, String.class, String.class, Boolean.TYPE,
+			String.class, String.class, String.class, String.class, Long.TYPE,
+			String.class, String.class, String.class, String.class,
+			String.class, String.class, String.class, String.class,
+			Integer.TYPE, Integer.TYPE, Boolean.TYPE, Integer.TYPE,
+			Integer.TYPE, Integer.TYPE, String.class, String.class,
+			String.class, String.class, String.class, String.class,
+			String.class, String.class, String.class, String.class,
+			String.class, Array.newInstance(Long.TYPE, 0).getClass(),
+			Array.newInstance(Long.TYPE, 0).getClass(),
+			Array.newInstance(Long.TYPE, 0).getClass(), List.class,
+			Array.newInstance(Long.TYPE, 0).getClass(),
+			ServiceContext.class
+		};
+	}
+
+	@Override
+	protected String[] getUpdateArgNames() {
+		return new String[] {
+			"userId", "oldPassword", "newPassword1", "newPassword2",
+			"passwordReset", "reminderQueryQuestion", "reminderQueryAnswer",
+			"screenName", "emailAddress", "facebookId", "openId", "languageId",
+			"timeZoneId", "greeting", "comments", "firstName", "middleName",
+			"lastName", "prefixId", "suffixId", "male", "birthdayMonth",
+			"birthdayDay", "birthdayYear", "smsSn", "aimSn", "facebookSn",
+			"icqSn", "jabberSn", "msnSn", "mySpaceSn", "skypeSn", "twitterSn",
+			"ymSn", "jobTitle", "groupIds", "organizationIds", "roleIds",
+			"userGroupRoles", "userGroupIds", "serviceContext"
+		};
+	}
+
+	@Override
+	protected Class<?> getUpdateClass() {
+		return UserLocalServiceUtil.class;
+	}
+
+	@Override
+	protected Map<String, Object> getUpdateEntrySpecifiedArgs(
+		Object entry, RequestContext requestContext) throws Exception {
+
+		return new HashMap<String, Object>(0);
+	}
+
+	@Override
+	protected String getUpdateMethodName() {
+		return "updateUser";
+	}
+
+}
